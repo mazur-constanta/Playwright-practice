6f370ec078b723eaa0a32a22007929b2214e4fb1
Constanta <constancegerman@gmail.com>
2024-09-15 17:08:24 +0300

    Added additional UI elements - 15.09

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -24,4 +24,59 @@ test.describe('form layouts page',  () => {
         // locator assertion 
         await expect(usingTheGridEmailInput).toHaveValue('clausmeine2001@yopmail.com');
     })
+
+    test('radio buttons', async ({ page }) => {
+        const usingTheGridForm = page.locator('nb-card', { hasText: "Using the Grid"});
+        
+        // await usingTheGridForm.getByLabel('Option 1').check({force: true})
+        await usingTheGridForm.getByRole('radio', { name: "Option 1"}).check({force: true})
+        const radioStatus = await usingTheGridForm.getByRole('radio', { name: "Option 1" }).isChecked()
+        expect(radioStatus).toBeTruthy()
+        await expect(usingTheGridForm.getByRole('radio', { name: "Option 1" })).toBeChecked()
+    })
+
+    test('checkbox', async ({ page }) => {
+        await page.getByText('Modal & Overlays').click()
+        await page.getByText('Toastr').click()
+
+        await page.getByRole('checkbox', { name: "Hide on click" }).check({ force: true }) // or can be used uncheck()
+
+        const allBoxes = page.getByRole('checkbox')
+        for(const box of await allBoxes.all()) {
+            await box.check({ force: true })
+            expect(await box.isChecked()).toBeTruthy() // or toBeFalsy()
+        }
+    })
+
+    test('List of dropdowns', async ({ page }) => {
+        const dropDownMenu = page.locator('ngx-header nb-select')
+        await dropDownMenu.click()
+
+        page.getByRole('list') // when the list has a UL tag
+        page.getByRole('listitem') // when the list has listitem tag
+
+        const optionList = page.locator('nb-option-list nb-option')
+        await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+        await optionList.filter({ hasText: "Cosmic" }).click()
+        const header = page.locator('nb-layout-header')
+        await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+        const colors = {
+            "Light": "rgb(255, 255, 255)",
+            "Dark": "rgb(34, 43, 69)",
+            "Cosmic": "rgb(50, 50, 89)",
+            "Corporate": "rgb(255, 255, 255)"
+        }
+
+        await dropDownMenu.click()
+        
+        for(const color in colors) {
+            await optionList.filter({ hasText: color }).click()
+            await expect(header).toHaveCSS('background-color', colors[color])
+
+            if(color != "Corporate") {
+                await dropDownMenu.click()
+            }
+        }
+    })    
 });
