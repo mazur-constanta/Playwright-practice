d1284b2b81ff97cbb306b407a797ead03c60e17a
Constanta <constancegerman@gmail.com>
2024-09-15 18:44:36 +0300

    UI Components added - 15.09 2nd part

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -69,7 +69,7 @@ test.describe('form layouts page',  () => {
         }
 
         await dropDownMenu.click()
-        
+
         for(const color in colors) {
             await optionList.filter({ hasText: color }).click()
             await expect(header).toHaveCSS('background-color', colors[color])
@@ -79,4 +79,55 @@ test.describe('form layouts page',  () => {
             }
         }
     })    
+
+    /** If some element is disappearing when we're trying to find its locator there is a need to:
+     * 1. Navigate to Sources tab
+     * 2. Hover on the element to show the tooltip
+     * 3. Click the combination Command + \ or F8 + Ctrl + \ to freeze the browser */
+
+    test('Tooltips', async ({ page }) => { 
+        await page.getByText('Modal & Overlays').click()
+        await page.getByText('Tooltip').click()
+
+        const toolTipCard = page.locator('nb-card', { hasText: "Tooltip Placements" })
+        await toolTipCard.getByRole('button', { name: "Top" }).hover()
+
+        page.getByRole('tooltip') // if you have a role tooltip created
+        const tooltip = await page.locator('nb-tooltip').textContent()
+        expect(tooltip).toEqual('This is a tooltip')
+    })
+
+    test('Dialog box', async ({ page }) => { 
+        await page.getByText('Tables & Data').click()
+        await page.getByText('Smart table').click()
+
+        page.on('dialog', dialog => {
+            expect(dialog.message()).toEqual('Are you sure you want to delete?')
+            dialog.accept()
+        })
+
+        await page.getByRole('table').locator('tr', { hasText: "mdo@gmail.com"}).locator('.nb-trash').click()
+        await expect(page.locator('table tr').first()).not.toHaveText("mdo@gmail.com")
+    })
+
+    test('Web tables', async ({ page }) => { 
+        await page.getByText('Tables & Data').click()
+        await page.getByText('Smart table').click()
+
+        // 1. get the row by any test in this row
+        const targetRow = page.getByRole('row', { name: "twitter@outlook.com" })
+        await targetRow.locator('.nb-edit').click()
+        await page.locator('input-editor').getByPlaceholder('Age').clear()
+        await page.locator('input-editor').getByPlaceholder('Age').fill('35')
+        await page.locator('.nb-checkmark').click()
+
+        // 2 get the row based on the value in the specific column
+        await page.locator('.ng2-smart-pagination-nav').getByText('2').click()
+        const targetRowById = page.getByRole('row', { name: "11" }).filter({ has: page.locator('td').nth(1).getByText('11')})
+        await targetRowById.locator('.nb-edit').click()
+        await page.locator('input-editor').getByPlaceholder('E-mail').clear()
+        await page.locator('input-editor').getByPlaceholder('E-mail').fill('test@test.com')
+        await page.locator('.nb-checkmark').click()
+        await expect(targetRowById.locator('td').nth(5)).toHaveText('test@test.com')
+    })
 });
